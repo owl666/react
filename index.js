31b8724712a44cb32b049fe1bec098769885ef38
owl666 <panda.hearts.penguin@gmail.com>
2018-02-23 11:41:40 +0200

    mongo + mongoose

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
-
-app.get('/', (req, res) => {
-	res.send('<h1>Emaily</h1>');
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
